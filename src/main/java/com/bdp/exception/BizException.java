95dfbda09713297967874ceb6ad8bc2b03cabe6b
hujing <23936987@qq.com>
2019-09-21 22:11:58 +0800

    bizException

--- src/main/java/com/bdp/exception/BizException.java
+++ src/main/java/com/bdp/exception/BizException.java
@@ -31,6 +31,22 @@ public class BizException extends Exception{
         this.errorCode = errorEnum.getErrorCode();
         this.errorMsg = errorMsg;
     }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+
+    public void setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
 }
 
 
